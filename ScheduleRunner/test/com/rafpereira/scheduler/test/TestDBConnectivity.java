a36b0426f102272dcd3832faa397a1cfd781d640
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-01-25 10:09:28 -0300

    added the option to multiple values for a process parameter.

--- ScheduleRunner/test/com/rafpereira/scheduler/test/TestDBConnectivity.java
+++ ScheduleRunner/test/com/rafpereira/scheduler/test/TestDBConnectivity.java
@@ -1,11 +1,15 @@
 package com.rafpereira.scheduler.test;
 
+import com.rafpereira.accesscontrol.business.util.EventTypeUtil;
 import com.rafpereira.accesscontrol.business.util.FeatureTypeUtil;
 import com.rafpereira.accesscontrol.data.util.AccessControlSessionFactoryUtil;
+import com.rafpereira.accesscontrol.model.EventType;
 import com.rafpereira.accesscontrol.model.FeatureType;
 import com.rafpereira.data.util.SessionFactoryUtil;
+import com.rafpereira.scheduler.business.util.ProcessTypeUtil;
 import com.rafpereira.scheduler.business.util.ScheduleOwnershipUtil;
 import com.rafpereira.scheduler.data.util.SchedulerSessionFactoryUtil;
+import com.rafpereira.scheduler.model.ProcessType;
 import com.rafpereira.scheduler.model.ScheduleOwnership;
 
 public class TestDBConnectivity {
@@ -15,18 +19,29 @@ public class TestDBConnectivity {
 		SessionFactoryUtil accessControlSessionFactory = new AccessControlSessionFactoryUtil();
 
 		ScheduleOwnershipUtil soUtil = new ScheduleOwnershipUtil();
-
 		for (ScheduleOwnership so : soUtil.listByFilter(null)) {
 			System.out.println(so.getId() + " " + so.getDescription());
 		}
 
+		ProcessTypeUtil ptUtil = new ProcessTypeUtil();
+		for (ProcessType pt : ptUtil.listByFilter(null)) {
+			System.out.println(pt.getId() + " " + pt.getDescription());
+		}
+		
+
+		
+		
 		
 		FeatureTypeUtil ftUtil = new FeatureTypeUtil();
-
 		for (FeatureType ft : ftUtil.listByFilter(null)) {
 			System.out.println(ft.getId() + " " + ft.getDescription());
 		}
-		
+
+		EventTypeUtil etUtil = new EventTypeUtil();
+		for (EventType et : etUtil.listByFilter(null)) {
+			System.out.println(et.getId() + " " + et.getDescription());
+		}
+
 		schedSessionFactory.close();
 		accessControlSessionFactory.close();
 	}
